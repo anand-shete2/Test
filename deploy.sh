38d6b3f64cdadecd55365ca1b2e5e198ca2b654d
Anand_Shete <anandshete1234@gmail.com>
2025-10-22 20:28:16 +0530

    done1

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-# nvm use 22
 
 echo "=== Deployment Started ==="
 
@@ -25,9 +24,10 @@ npm ci --omit=dev
 mv ~/.env .
 
 
-# Cleanup old processes
-rm -rf ~/Test
+# Cleanup step
+rm -rf ~/Test ~/deploy.sh
 pm2 delete test || true
+
 
 
 # Start new processes
